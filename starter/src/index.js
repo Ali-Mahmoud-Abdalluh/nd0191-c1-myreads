6ce39966766b34ecdea6d449a1612a9d706f7d16
Ali-Mahmoud-Abdalluh <alimahmoudabdallaemam12@gmail.com>
2022-09-18 23:06:21 +0200

    Preparing the project to upload it in the form of an interactive page on github 2

--- starter/src/index.js
+++ starter/src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter  basename="/nd0191-c1-myreads">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
